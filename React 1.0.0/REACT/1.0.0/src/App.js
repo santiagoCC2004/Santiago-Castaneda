00e828faf1ac61767deae39dfbef500a93cbb8b3
santiagoCC2004 <132492590+santiagoCC2004@users.noreply.github.com>
2023-07-27 19:45:25 -0500

    Add files via upload

---
+++ React 1.0.0/REACT/1.0.0/src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+
+function App() {
+  return (
+    <div classNameName="App">
+      <header className="App-header">
+        <div className="container">
+          <div className="row">
+            <div className="cold-md-6">
+              <div className="alert alert-primary" role="alert">
+                "¿Sabes cómo se dice "pollo" en japonés? ¡Pollogram!"
+              </div>
+              <div className="alert alert-secondary" role="alert">
+                "Estoy en una relación a distancia. Mi novia está en el futuro, pero todo va bien".
+              </div>
+              <div className="alert alert-success" role="alert">
+                "¿Por qué los pájaros no usan Facebook? Porque ya tienen Twitter".
+              </div>
+              <div className="alert alert-danger" role="alert">
+                "Estaba pensando en perder peso, pero me di cuenta de que no quiero desperdiciar una vida perfectamente buena en una dieta".
+              </div>
+              <div className="alert alert-warning" role="alert">
+                "¿Qué hace una abeja en el gimnasio? ¡Zum-ba!"
+              </div>
+              <div className="alert alert-info" role="alert">
+                "Mi vecino toca la batería las 24 horas del día... incluso en la ducha. No sé cómo, pero puedo escuchar el tamborileo".
+              </div>
+              <div className="alert alert-light" role="alert">
+                "¿Por qué los esqueletos no pelean entre ellos? Porque no tienen agallas".
+              </div>
+              <div className="alert alert-dark" role="alert">
+                "Le dije a mi perro que hablaría con él en lenguaje de señas, pero aún no sé cómo decir 'galleta' con las patas".
+              </div>
+            </div>
+            <div className="cold-md-6"></div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
